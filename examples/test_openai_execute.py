d0872773d351a1aa9559c9e9f0cfa4d4c350bafb
Benjamin Swerdlow <Swerdlowbenjamin@gmail.com>
2025-03-10 15:18:39 -0700

    wrote more docs

--- examples/test_openai_execute.py
+++ examples/test_openai_execute.py
@@ -2,18 +2,20 @@ import os
 import openai
 import freestyle.openai
 
-client = openai.OpenAI(
-    api_key=os.environ.get("OPENAI_API_KEY"),
-)
-
 
 (definition, runner) = freestyle.openai.execute_tool(
     os.environ.get("FREESTYLE_API_KEY"),
+    freestyle.FreestyleExecuteScriptParamsConfiguration(
+        nodeModules={"mathjs": "14.3.1"},
+    ),
 )
 
 query = "What is the sum of every number from 50 to 65 divided by 17"
 messages = [{"role": "user", "content": query}]
 print("Question: ", query)
+client = openai.OpenAI(
+    api_key=os.environ.get("OPENAI_API_KEY"),
+)
 
 res = client.chat.completions.create(
     model="gpt-4-turbo", messages=messages, tools=[definition]
